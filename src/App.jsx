8c9c75bb323601088b46a349fc802928754b142a
Anurag <anurag-kumar.yadav@capgemini.com>
2024-12-14 17:55:35 +0530

    Added BankerHome Route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Landing from "./components/Landing";
 import OpenAccount from "./components/OpenAccount";
 import Login from "./components/Login";
 import BankerLogin from "./components/BankerLogin";
+import BankerHome from "./components/BankerHome";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Route path="/openAccount" element={<OpenAccount />} />
           <Route path="/login" element={<Login />} />
           <Route path="/bankerLogin" element={<BankerLogin />} />
+          <Route path="/bankerHome" element={<BankerHome />} />
         </Routes>
       </BrowserRouter>
     </>
