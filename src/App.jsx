f43ed649a01971d007f7ad9fb1a1e71f0e6862b9
Anurag <anurag-kumar.yadav@capgemini.com>
2024-12-18 17:11:03 +0530

    Added user route

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import Login from "./components/Login";
 import BankerLogin from "./components/BankerLogin";
 import BankerHome from "./components/BankerHome";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Root from "./components/Root";
+import Profile from "./components/Profile";
+import Home from "./components/Home";
 
 function App() {
   return (
@@ -16,6 +19,10 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/bankerLogin" element={<BankerLogin />} />
           <Route path="/bankerHome" element={<BankerHome />} />
+          <Route path="/root/*" element={<Root />}>
+            <Route path="" element={<Home />} />
+            <Route path="profile" element={<Profile />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
